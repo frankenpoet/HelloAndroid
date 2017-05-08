56b47d015824a46c5a3cfc527592ed00df1eb297
SWicken2151 <frankenpoet@gmail.com>
2017-05-08 10:55:34 -0400

    added buttons

--- app/src/main/java/com/example/swicken2151/helloandroid/MyActivity.java
+++ app/src/main/java/com/example/swicken2151/helloandroid/MyActivity.java
@@ -2,6 +2,9 @@ package com.example.swicken2151.helloandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        // Let's take a look at Toast and Log in action
+        Toast.makeText(this, "Can you see me?",
+            Toast.LENGTH_SHORT).show();
+
+        Log.i("info", "Done creating the app");
+    }
+
+    public void topClick(View v){
+        Toast.makeText(this, "Top button clicked",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user clicked the top button");
+    }
+
+    public void bottomClick(View v){
+        Toast.makeText(this, "Bottom button clicked",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user clicked the bottom button");
     }
 }
